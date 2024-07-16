43c3192ced660ba5409e9bd3f8cb52f0b9cfc6d0
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2024-07-15 22:49:47 -0500

    Added a query parameter endpoint.

--- simple_server.py
+++ simple_server.py
@@ -9,10 +9,20 @@ class courseRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+class queryParamRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        number = self.get_argument("num")
+        if (number.isdigit()):
+            result = "odd" if int(number) % 2 else "even"
+            self.write(f"{number} is {result}")
+        else:
+            self.write("Invalid input")
+
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/", basicRequestHandler),
         (r"/courses", courseRequestHandler),
+        (r"/isEven", queryParamRequestHandler),
 
     ])
 
